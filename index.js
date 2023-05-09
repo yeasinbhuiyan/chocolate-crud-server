4d8d80cd21f32d187d5eee8a8b3154ad51fa948a
yeasinbhuiyan <yeasin382255656@gmail.com>
2023-05-09 19:14:50 +0600

    server side first step complete

--- index.js
+++ index.js
@@ -1,20 +1,80 @@
 
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const express = require('express');
- const cors = require('cors');
- const app = express()
+const cors = require('cors');
+require('dotenv').config()
 
- const port = process.env.PORT || 5000
+const app = express()
+
+const port = process.env.PORT || 5000
 
 
 //  middlewear
- app.use(cors())
- app.use(express.json())
+app.use(cors())
+app.use(express.json())
 
 
- app.get('/',(req,res)=>{
+
+
+
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.9xgdj4e.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        const database = client.db("chocolatesDB");
+        const chocolateCollection = database.collection("Chocolates");
+
+
+        app.get('/chocolates',async(req,res)=>{
+            const chocolate = chocolateCollection.find()
+            const result = await chocolate.toArray()
+            res.send(result)
+        })
+
+
+        app.post('/chocolates', async (req, res) => {
+            const chocolate = req.body
+            const result = await chocolateCollection.insertOne(chocolate)
+            res.send(result)
+        })
+
+        app.delete('/delete/:id',async(req,res)=>{
+            const id = req.params.id 
+            const filter = {_id : new ObjectId(id)}
+            const result = await chocolateCollection.deleteOne(filter)
+            res.send(result)
+
+        })
+
+
+
+
+        // Send a ping to confirm a successful connection
+        // await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
     res.send('This Is Chocolate Server')
- })
+})
 
- app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`This Server Port is ${port}`)
- })
+})
